07e93ad579be44f8e43cb32ddaafd4b769e26db1
isaranagati953-blip <isaranagati953@gmail.com>
2026-06-20 01:46:30 +0530

    removed unnecessary cope:

--- test_jose.js
+++
@@ -1,3 +0,0 @@
-const { SignJWT } = require('jose');
-const jwt = new SignJWT({}).setProtectedHeader({ alg: "HS256" }).setIssuedAt().setExpirationTime("3d");
-console.log("Works!");
--- test_login.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-  const res = await fetch("http://localhost:3000/api/auth/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email: "isaranagati953@email.com", password: "iS@ran@gat!953#" })
-  });
-  console.log("Status:", res.status);
-  console.log("Body:", await res.json());
-})();
